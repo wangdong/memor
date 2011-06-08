3e06f602b5b779f3441197daced5a54645b4a654
Dong Wang <wangdong2@gmail.com>
2011-06-08 21:25:24 +0800

    Initial Commit

---
+++ memorTests/memorTests.h
@@ -0,0 +1,17 @@
+//
+//  memorTests.h
+//  memorTests
+//
+//  Created by Wang Dong on 6/8/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface memorTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ memorTests/memorTests.m
@@ -0,0 +1,33 @@
+//
+//  memorTests.m
+//  memorTests
+//
+//  Created by Wang Dong on 6/8/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "memorTests.h"
+
+
+@implementation memorTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in memorTests");
+}
+
+@end
