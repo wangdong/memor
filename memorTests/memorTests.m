9e97faab6dfa886b9a8030eb0271991ddad4df86
Dong Wang <wangdong2@gmail.com>
2011-06-09 23:39:49 +0800

    unittest added

--- memorTests/memorTests.m
+++ memorTests/memorTests.m
@@ -75,8 +75,7 @@
 
 - (void)tearDown
 {
-    // Tear-down code here.
-    
+    [[NaAtomStore sharedStore] reset];
     [super tearDown];
 }
 
@@ -100,9 +99,115 @@
     STAssertEquals(@"Hello, AtomStore!", [atom msg], @"");
     [store undo];
     STAssertEquals(@"Hello, World!", [atom msg], @"");
+    
+    
+    //
+    // undo/redoÂà∞Â∞ΩÂ§¥ÂêéÁöÑË°å‰∏∫
+    //
+    STAssertFalse([store canUndo], @"");
+    [store undo];
+    [store undo];
+    [store undo];
+    STAssertEquals(@"Hello, World!", [atom msg], @"");
+    
+    [store redo];
+    STAssertEquals(@"Hello, AtomStore!", [atom msg], @"");
+    [store redo];
+    STAssertEquals(@"Hello, AtomImage!", [atom msg], @"");
+    
+    STAssertFalse([store canRedo], @"");
+    [store redo];
+    [store redo];
+    [store redo];
+    STAssertEquals(@"Hello, AtomImage!", [atom msg], @"");
+    
+    //
+    // redoÂêéÂèàÊèê‰∫§‰ºö‰∏¢ÂºÉÊéâÂâçÈù¢ÁöÑÂÜÖÂÆπ‰ªéËÄåÊó†Ê≥ïÂÜçredoÂõûÂéª
+    //
+    [store undo];
+    STAssertEquals(@"Hello, AtomStore!", [atom msg], @"");
+    [atom putMsg:@"Hello, RedoRedo!"];
+    [store commit];
+    
+    STAssertFalse([store canRedo], @"");
+    STAssertEquals(@"Hello, RedoRedo!", [atom msg], @"");
 
     
     [pool drain];
 }
+
+
+-(void) testMaxSteps {
+    NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
+    
+    NaAtomStore* store = [NaAtomStore sharedStore];
+    STAssertEquals(DEFAULT_MAX_UNDO_STEPS, [store maxSteps], @"");
+    
+    MyAtom* atom =  [[[MyAtom alloc] init] autorelease];
+    
+    [atom putMsg:@"Hello, World!"];
+    [store commit];
+    
+    for (int i = 0; i < 20; ++i) {
+        [atom putMsg:@"Hello, CAP!"];
+        [store commit];
+    }
+    
+    for (int i = 0; i < 20; ++i)
+        [store undo];
+    STAssertFalse([store canUndo], @"");
+    STAssertEquals(@"Hello, CAP!", [atom msg], @"");
+    
+    [store setMaxSteps: 3];
+    [store redo];
+    [store redo];
+    [store redo];
+    [store redo];
+    STAssertTrue([store canRedo], @"");
+
+    [store undo];
+    [store undo];
+    [store undo];
+    STAssertFalse([store canUndo], @"");
+    
+    [store setMaxSteps:50];
+    for (int i = 0; i < 50; ++i) {
+        [atom putMsg: @"Hello, CAP!"];
+        [store commit];
+    }
+    for (int i = 0; i < 50; ++i)
+        [store undo];
+    STAssertFalse([store canUndo], @"");
+    
+    [pool drain];
+}
+
+
+-(void) testReset {
+    NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
+    
+    NaAtomStore* store = [NaAtomStore sharedStore];
+
+    MyAtom* atom = [[MyAtom alloc] init];
+    [atom putMsg:@"ABC"];
+    [store commit];
+    [atom putMsg:@"DEF"];
+    [store commit];
+    [atom putMsg:@"HIG"];
+    [store commit];
+    
+    [store undo];
+    STAssertTrue([store canRedo], @"");
+    STAssertTrue([store canUndo], @"");
+
+    [store reset];
+    
+    STAssertFalse([store canRedo], @"");
+    STAssertFalse([store canUndo], @"");
+    STAssertEquals(DEFAULT_MAX_UNDO_STEPS, [store maxSteps], @"");
+    
+    [pool drain];
+}
+
 
 @end
